940c50f4c7d9bf6f22f75e431e345bb75c5a67e7
Washington-Ksea <ksea.washington@gmail.com>
2019-08-04 03:27:40 +0900

    BookオブジェクトのCRUDを実行するAPIviewを作成

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from shop1.models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """BookオブジェクトのCRUDを実行するAPI"""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
