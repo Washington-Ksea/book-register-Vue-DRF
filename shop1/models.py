56cd790e6f60ecbe8d154f3bbd1340431bea740f
Washington-Ksea <ksea.washington@gmail.com>
2019-08-04 02:54:03 +0900

    本のモデルを追加

--- shop1/models.py
+++ shop1/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
+class Book(models.Model):
+    """本のモデル"""
+    class Meta:
+        db_table = 'book'
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid64, editable=False)
+    title = models.CharField(_('タイトル'), max_length=20)
+    price = models.DecimalField(_('価格'), null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
+    
